e67f1cda0feb8b76ca481ec6d63d86fe6ff59337
Jeff Licona <57239224+jeffala@users.noreply.github.com>
2022-05-04 20:29:10 -0400

    Linked lists #2

--- 06-linked-list/projects/starter/LinkedList.playground/Contents.swift
+++ 06-linked-list/projects/starter/LinkedList.playground/Contents.swift
@@ -2,13 +2,24 @@
 // For full license & permission details, see LICENSE.markdown.
 
 
-example(of: "creating and linking nodes") {
-    let node1 = Node(value: 1)
-    let node2 = Node(value: 2)
-    let node3 = Node(value: 3)
+//example(of: "creating and linking nodes") {
+//    let node1 = Node(value: 1)
+//    let node2 = Node(value: 2)
+//    let node3 = Node(value: 3)
+//
+//    node1.next = node2
+//    node2.next = node3
+//
+//    print(node1)
+//}
+
+
+
+example(of: "push") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
     
-    node1.next = node2
-    node2.next = node3
-    
-    print(node1)
+    print(list)
 }
